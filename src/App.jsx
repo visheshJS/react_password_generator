5dbce527508c8e70de6f0ed944d58821a61ef506
visheshJS <visheshsharma_23me287@dtu.ac.in>
2025-01-12 03:23:43 +0530

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -39,60 +39,62 @@ function App() {
   }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
     
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-      <h1 className='text-white text-center my-3'>Password generator</h1>
-    <div className="flex shadow rounded-lg overflow-hidden mb-4">
-        <input
-            type="text"
-            value={password}
-            className="outline-none w-full py-1 px-3"
-            placeholder="Password"
-            readOnly
-            ref={passwordRef}
-        />
-        <button
-        onClick={copyPasswordToClipboard}
-        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 
-        active:bg-blue-700 hover:bg-blue-400 '
-        >copy</button>
-        
-    </div>
-    <div className='flex text-sm gap-x-2'>
-      <div className='flex items-center gap-x-1'>
-        <input 
+    <div className="w-full sm:w-11/12 md:w-[600px] mx-auto shadow-md rounded-lg px-6 py-8 my-10  bg-gray-800 text-gray-900 max-w-full">
+  <h1 className='text-yellow-200 text-4xl text-center mb-10 my-3'>Password generator...</h1>
+  <div className="flex shadow rounded-lg overflow-hidden mb-4">
+    <input
+      type="text"
+      value={password}
+      className="outline-none w-full py-4 text-xl px-3"
+      placeholder="Password"
+      readOnly
+      ref={passwordRef}
+    />
+    <button
+      onClick={copyPasswordToClipboard}
+      className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 
+      active:bg-blue-700 hover:bg-blue-400 text-lg '
+    >Copy</button>
+  </div>
+  <div className='flex flex-col sm:flex-row sm:gap-x-2 text-sm gap-y-3 sm:gap-y-0'>
+    <div className='flex  items-center gap-x-1 w-full sm:w-auto'>
+    <label className='text-white text-lg'>Length:</label>
+    <label className='text-white mx-2 text-lg'>{length}</label>
+      <input 
         type="range"
         min={6}
         max={100}
         value={length}
-         className='cursor-pointer'
-         onChange={(e) => {setLength(e.target.value)}}
-          />
-          <label>Length: {length}</label>
-      </div>
-      <div className="flex items-center gap-x-1">
-      <input
-          type="checkbox"
-          defaultChecked={numberAllowed}
-          id="numberInput"
-          onChange={() => {
-              setNumberAllowed((prev) => !prev);
-          }}
+        className='cursor-pointer w-full'
+        onChange={(e) => { setLength(e.target.value) }}
       />
-      <label htmlFor="numberInput">Numbers</label>
-      </div>
-      <div className="flex items-center gap-x-1">
-          <input
-              type="checkbox"
-              defaultChecked={charAllowed}
-              id="characterInput"
-              onChange={() => {
-                  setCharAllowed((prev) => !prev )
-              }}
-          />
-          <label htmlFor="characterInput">Characters</label>
-      </div>
+       <span className="text-white text-lg">{length}</span>
     </div>
+    <div className="flex items-center gap-x-1 w-full sm:w-auto">
+      <input
+        type="checkbox"
+        defaultChecked={numberAllowed}
+        id="numberInput"
+        onChange={() => {
+          setNumberAllowed((prev) => !prev);
+        }}
+      />
+      <label className='text-white text-lg' htmlFor="numberInput">Numbers</label>
+    </div>
+    <div className="flex items-center gap-x-1 w-full sm:w-auto">
+      <input
+        type="checkbox"
+        defaultChecked={charAllowed}
+        id="characterInput"
+        onChange={() => {
+          setCharAllowed((prev) => !prev)
+        }}
+      />
+      <label className='text-white text-lg' htmlFor="characterInput">Characters</label>
+    </div>
+  </div>
 </div>
+
     
   )
 }
